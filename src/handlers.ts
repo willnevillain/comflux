f2256b69cde6b3e8dd87c3cc53a401c44115ed2b
Will Neville <willnevillain@gmail.com>
2021-06-06 16:57:53 -0400

    feat: can subscribe to channel update eventsub and clean up subscriptions

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,8 +1,29 @@
 import axios from 'axios';
-import { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } from './config';
+import crypto from 'crypto';
+import { Request } from 'express';
+import { TwitchVerificationInfo } from '.';
+import {
+    NGROK_URL,
+    TWITCH_CLIENT_ID,
+    TWITCH_CLIENT_SECRET,
+    TWITCH_EVENTSUB_SECRET,
+} from './config';
 
 type TwitchAuthResponse = {
     access_token: string;
+};
+
+type TwitchCreateSubscriptionBody = {
+    type: string;
+    version: string;
+    condition: {
+        broadcaster_user_id: string;
+    };
+    transport: {
+        method: string;
+        callback: string;
+        secret: string;
+    };
 };
 
 type TwitchUserDetail = {
@@ -19,7 +40,7 @@ type TwitchUserDetail = {
     started_at: string;
 };
 
-export const handleGetWill = async (): Promise<TwitchUserDetail> => {
+const getTwitchAccessToken = async (): Promise<TwitchAuthResponse> => {
     const authResponse = await axios({
         url: 'https://id.twitch.tv/oauth2/token',
         method: 'POST',
@@ -30,7 +51,102 @@ export const handleGetWill = async (): Promise<TwitchUserDetail> => {
         },
     });
 
-    const authData = authResponse.data as TwitchAuthResponse;
+    return authResponse.data as TwitchAuthResponse;
+};
+
+export const verifyTwitchSignature = async ({
+    messageId,
+    messageSignature,
+    messageTimestamp,
+    rawRequestBody,
+}: TwitchVerificationInfo): Promise<boolean> => {
+    const message = `${messageId}${messageTimestamp}${rawRequestBody}`;
+    const signature = crypto
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        .createHmac('sha256', TWITCH_EVENTSUB_SECRET!)
+        .update(message);
+    const expected = `sha256=${signature.digest('hex')}`;
+    console.log(messageSignature);
+    console.log(expected);
+    return expected === messageSignature;
+};
+
+export const handleChannelUpdateEvent = async (body: any): Promise<void> => {
+    console.log(body.event);
+};
+
+export const handleDeleteEventSubscriptions = async (): Promise<void> => {
+    const { access_token } = await getTwitchAccessToken();
+    const { data: responseBody } = await axios({
+        url: 'https://api.twitch.tv/helix/eventsub/subscriptions',
+        method: 'GET',
+        headers: {
+            'Client-ID': TWITCH_CLIENT_ID,
+            Authorization: `Bearer ${access_token}`,
+        },
+    });
+
+    const subscriptions = responseBody.data as { id: string }[];
+
+    console.log(JSON.stringify(subscriptions));
+
+    await Promise.all(
+        subscriptions.map((subscription) =>
+            axios({
+                url: 'https://api.twitch.tv/helix/eventsub/subscriptions',
+                method: 'DELETE',
+                params: {
+                    id: subscription.id,
+                },
+                headers: {
+                    'Client-ID': TWITCH_CLIENT_ID,
+                    Authorization: `Bearer ${access_token}`,
+                },
+            })
+        )
+    );
+
+    const { data: subscriptionsAfterDelete } = await axios({
+        url: 'https://api.twitch.tv/helix/eventsub/subscriptions',
+        method: 'GET',
+        headers: {
+            'Client-ID': TWITCH_CLIENT_ID,
+            Authorization: `Bearer ${access_token}`,
+        },
+    });
+    console.log(JSON.stringify(subscriptionsAfterDelete));
+};
+
+export const handleCreateChannelUpdateSubscription =
+    async (): Promise<void> => {
+        const { access_token } = await getTwitchAccessToken();
+        const body: TwitchCreateSubscriptionBody = {
+            type: 'channel.update',
+            version: '1',
+            condition: {
+                broadcaster_user_id: '682923024',
+            },
+            transport: {
+                method: 'webhook',
+                callback: `${NGROK_URL}/eventsub`,
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                secret: TWITCH_EVENTSUB_SECRET!,
+            },
+        };
+        await axios({
+            url: 'https://api.twitch.tv/helix/eventsub/subscriptions',
+            method: 'POST',
+            headers: {
+                'Client-ID': TWITCH_CLIENT_ID,
+                Authorization: `Bearer ${access_token}`,
+                'Content-Type': 'application/json',
+            },
+            data: body,
+        });
+    };
+
+export const handleGetWill = async (): Promise<TwitchUserDetail> => {
+    const { access_token } = await getTwitchAccessToken();
 
     const apiResponse = await axios({
         url: 'https://api.twitch.tv/helix/search/channels',
@@ -39,7 +155,7 @@ export const handleGetWill = async (): Promise<TwitchUserDetail> => {
         },
         headers: {
             'client-id': TWITCH_CLIENT_ID,
-            Authorization: `Bearer ${authData.access_token}`,
+            Authorization: `Bearer ${access_token}`,
         },
     });
 
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import express from 'express';
-import { handleGetWill } from './handlers';
+import { json as bodyParserJson } from 'body-parser';
+import express, { Request, Response } from 'express';
+import { IncomingMessage } from 'http';
+import {
+    handleChannelUpdateEvent,
+    handleCreateChannelUpdateSubscription,
+    handleDeleteEventSubscriptions,
+    handleGetWill,
+    verifyTwitchSignature,
+} from './handlers';
 
 const app = express();
+
+app.use(
+    bodyParserJson({
+        verify: (req: IncomingMessage & { rawBody?: Buffer }, _res, buf) => {
+            req.rawBody = buf;
+        },
+    })
+);
+
 const port = 3000;
 
 app.get('/health', (_req, res) => {
@@ -11,6 +28,53 @@ app.get('/health', (_req, res) => {
 app.get('/users/will', async (_req, res) => {
     const will = await handleGetWill();
     res.status(200).send(will);
+});
+
+app.post('/eventsub/createChannelUpdateSubscription', async (_req, res) => {
+    await handleCreateChannelUpdateSubscription();
+    res.status(200).send({});
+});
+
+const isCallbackVerificationRequest = (req: Request): boolean =>
+    req.header('twitch-eventsub-message-type') ===
+    'webhook_callback_verification';
+
+export type TwitchVerificationInfo = {
+    messageId: string;
+    messageSignature: string;
+    messageTimestamp: string;
+    rawRequestBody: Buffer;
+};
+
+const extractVerificationInfo = (req: Request): TwitchVerificationInfo => {
+    const messageId = req.header('twitch-eventsub-message-id');
+    const messageSignature = req.header('twitch-eventsub-message-signature');
+
+    const messageTimestamp = req.header('twitch-eventsub-message-timestamp');
+    if (!messageId || !messageSignature || !messageTimestamp) {
+        throw new Error('Missing required headers - was the call from Twitch?');
+    }
+    const rawRequestBody = Buffer.from(JSON.stringify(req.body));
+    return { messageId, messageSignature, messageTimestamp, rawRequestBody };
+};
+
+app.post('/eventsub', async (req: Request, res: Response) => {
+    if (isCallbackVerificationRequest(req)) {
+        const verificationInfo = extractVerificationInfo(req);
+        const isValid = verifyTwitchSignature(verificationInfo);
+        if (!isValid) {
+            res.status(403).send({ error: 'Invalid signature' });
+        }
+        res.status(200).send(req.body.challenge);
+    } else {
+        await handleChannelUpdateEvent(req.body);
+        res.status(200).send({});
+    }
+});
+
+app.delete('/eventsub', async (_req: Request, res: Response) => {
+    await handleDeleteEventSubscriptions();
+    res.status(204).send({});
 });
 
 app.listen(port, () => {
