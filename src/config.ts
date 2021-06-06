09ebed480e64c0d6509f596adf743d50df7864e7
Will Neville <willnevillain@gmail.com>
2021-06-06 16:06:43 -0400

    chore: add body-parser dependency, add additional env vars

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,5 @@ dotenv.config({ path: DOT_ENV });
 
 export const TWITCH_CLIENT_ID = process.env.TWITCH_CLIENT_ID;
 export const TWITCH_CLIENT_SECRET = process.env.TWITCH_CLIENT_SECRET;
+export const NGROK_URL = process.env.NGROK_URL;
+export const TWITCH_EVENTSUB_SECRET = process.env.TWITCH_EVENTSUB_SECRET;
